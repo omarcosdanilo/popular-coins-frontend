4910ef4b4c5b317869299df3da0bca21c3067763
Marcos Danilo <marcosdanilo94@gmail.com>
2023-10-11 16:45:54 -0300

    feat: render the Coins component
    the Coins component render the coins on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Coins from './components/Coins'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>Hello World</div>
+    <Coins />
   )
 }
 
