a5ea9b05998bfb3b17c434368f2e5356aa8228c8
Marcos Danilo <marcosdanilo94@gmail.com>
2023-10-11 16:47:57 -0300

    feat: create an interface for the database response

---
+++ src/interfaces/ICoin.ts
@@ -0,0 +1,9 @@
+export interface ICoin {
+  "icon": "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg",
+  "id": "Qwsogvtv82FCd",
+  "name": "Bitcoin",
+  "number_of_reviews": 0,
+  "price": "27462.962616586206",
+  "symbol": "BTC",
+  "total_score": 0
+}
